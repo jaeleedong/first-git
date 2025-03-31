3a101618eb76e11bfa912ab63395816271a7717e
leedongjae <dj8@naver.com>
2025-03-31 14:49:23 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+    int[] a = new int[45];
+
+    for(int i =0; i < a.length; i++){
+        a[i]= i +1;
+    }
+
+    for(int i =0; i <6; i++){
+
+    }
+
+        System.out.println(Arrays.toString(a));
+    }
+}
---
+++ src/c16.java
@@ -0,0 +1,73 @@
+public class c16 {
+    public static void main(String[] args) {
+        int a = 0 , b = 0, c=0;
+        for(int i=0; i<3; i++){
+            for(int k=0; k<a; k++){
+                System.out.print(".");
+            }
+            for(int j=4; j>=b; j--) {
+                System.out.print("*");
+            }
+            a = a+1;
+            b = b+2;
+            System.out.println();
+        }
+        for(int i=0; i<3; i++){
+            for(int j=1; j>i; j--) {
+                System.out.print(".");
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //        int a= 1;
+//        for(int i=0; i <5; i++){
+//            for(int j=4; j > i; j--){
+//                System.out.print(".");
+//            }
+//            for(int k=0; k<a; k++){
+//                System.out.print("#");
+//            }
+//            a +=2;
+//            System.out.println();
+//8        }
+
+
+//        for(int j =0; j < 5; j++){
+//            for(int k=4; k>j; k--){
+//                System.out.print(".");
+//            }
+//            for(int i=0; i<=j; i++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+// 6        }
+    }
+}
+
+
+
+
+
+//        for(int j =1; j<=5; j++){
+//                for(int k=1; k<=j; k++){
+//                    System.out.print(" ");
+//                }
+//                System.out.println("*");
+//            }
+//
+//        for(int i =5; i>=1; i--){
+//            for(int j=1; j<=i; j++){
+//                System.out.print(" ");
+//            }
+//            System.out.println("*");
+//        }
+
+
