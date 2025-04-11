0f90818d7785fcd974280ed5140813a46d73c315
dongjae <dj8@naver.com>
2025-04-11 17:02:00 +0900

    d

--- src/E6_23.java
+++ src/E6_23.java
@@ -1,5 +1,4 @@
 public class E6_23 {
-
     static int max(int [] arr) {
         try {
             for (int i = 0; i < arr.length; i++) {
@@ -13,12 +12,13 @@ public class E6_23 {
             return -99999;
         }
     }
-
     public static void main(String[] args) {
         int[] data = {3,2,9,4,7};
         System.out.println(java.util.Arrays.toString(data));
         System.out.println(" 최대값:"+max(data));
         System.out.println(" 최대값:"+max(null));
         System.out.println(" 최대값:"+max(new int[]{})); // 0 최대값 크기가 인 배열
+        System.out.println(java.util.Arrays.toString(data));
+
     }
 }
